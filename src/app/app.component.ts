33e25d66b6b411e7bc140256fa62d649241bd100
lanhnth-1450 <nguyen.thi.huyen.lanh@sun-asterisk.com>
2021-03-22 00:07:43 +0700

    save data from form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
 import { Habit } from './models/habit';
 
 @Component({
@@ -9,6 +10,14 @@ import { Habit } from './models/habit';
 
 export class AppComponent {
   public adding = false;
+  public editing = false;
+  public editingIndex: number = 0;
+
+  public habitForm = new FormGroup({
+    name: new FormControl(''),
+    frequency: new FormControl(''),
+    description: new FormControl(''),
+  });
 
   public habits: Habit[] = [
     <Habit>{
@@ -24,4 +33,27 @@ export class AppComponent {
         'The weeds get so out of hand if they wait any longer, and I like how nice our home looks with a clean lawn.',
     },
   ];
+
+  public onSubmit() {
+    const habit = this.habitForm.value as Habit;
+
+    if (this.editing) {
+      this.habits.splice(this.editingIndex, 1, habit);
+    } else {
+      this.habits.push(habit);
+    }
+
+    this.editing = false;
+    this.adding = false;
+  }
+
+  public setEditForm(habit: Habit, index: number) {
+    this.habitForm.patchValue({
+      name: habit.name,
+      frequency: habit.frequency,
+      description: habit.description,
+    });
+    this.editing = true;
+    this.editingIndex = index;
+  }
 }
